3e134d8bb87412d3ba019371ed6ae1ae8c73f94a
Paul Sammut <paul.c.sammut@gmail.com>
2016-12-11 22:36:20 -0500

    ok start point for encoder integration

--- firmware/arduino_main/LTP_firmware/LTP_firmware.ino
+++ firmware/arduino_main/LTP_firmware/LTP_firmware.ino
@@ -9,22 +9,26 @@ void setup() {
   
   lidar.init();
   // We must specify a packet handler method so that
-  serial.setPacketHandler(&onPacket);
+  // serial.setPacketHandler(&onPacket);
   serial.begin(115200);
+
   Serial.println("Booted up!");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  serial.update();
+  // serial.update();
 
-  const uint8_t packet1 = lidar.getDistance();
-  serial.send(&packet1, 1);
+  //const uint8_t packet1 = lidar.getDistance();
+  //serial.send(&packet1, 1);
 
-  delay(2);
+  delay(1000);
+
+  Serial.println("beep boop");
   
 }
+
 
 void onPacket(const uint8_t* buffer, size_t size)
 {
