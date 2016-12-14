5ac67c866a4a821e1b73f32ab7a740c3eac26988
Paul Sammut <paul.c.sammut@gmail.com>
2016-12-13 23:53:53 -0500

    Firmware has motorcontroller PWM implement. Shaky, getting >nack errors on LIDAR read.

--- firmware/arduino_main/LTP_firmware/LTP_firmware.ino
+++ firmware/arduino_main/LTP_firmware/LTP_firmware.ino
@@ -8,15 +8,30 @@ LIDAR lidar;
 PacketSerial serial;
 Encoder encoder;
 
+int motorDuty;
+int motorDutyPeriod = 1000; //period in us
+unsigned long motorDutyLastTime;
+unsigned long motorDutyEndTime =0;
+int motorPin = 6;
+
+
+
 void setup() {
+
+  //pinMode(motorPin, OUTPUT);
+  //digitalWrite(motorPin, LOW);
+  
   
   lidar.init();
+  
   encoder.init();
+  
   // We must specify a packet handler method so that
   serial.setPacketHandler(&onPacket);
   serial.begin(115200);
   
   Serial.println("Booted up!");
+  setMotorSpeed(50);
 
   
 
@@ -29,11 +44,13 @@ void loop() {
   const uint8_t packet1 = lidar.getDistance();
   //serial.send(&packet1, 1);
 
-  //delay(10);
+  
   Serial.print(packet1);
   Serial.print(",");
   Serial.println(encoder.getAngle());
-  
+
+  analogWrite(6, 50);
+  //generateMotorSig();
 }
 
 
@@ -42,5 +59,23 @@ void onPacket(const uint8_t* buffer, size_t size)
   uint8_t string[17] = "Packet received!";
   
   serial.send(string, 16);
+}
+
+void generateMotorSig()
+{
+  unsigned long curTime = micros();
+  if(curTime - motorDutyLastTime > motorDutyEndTime)
+  {
+    //Serial.println("Toggling! ");
+    digitalWrite(motorPin, !digitalRead(motorPin));
+    motorDutyLastTime = curTime;
+  }
+}
+
+void setMotorSpeed(int motorSpeed)
+{
+  motorDutyEndTime = (double)(motorSpeed)*0.01*motorDutyPeriod;
+  Serial.print("MotorDuty end time is: ");
+  Serial.println(motorDutyEndTime);
 }
 
