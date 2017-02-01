6aa42f9362f2108d4125b6dfc15e5f840bb8a319
Paul Sammut <paul.c.sammut@gmail.com>
2017-02-01 12:17:26 -0500

    Adding PID files

--- firmware/LTP_Board/LTP_Board_V1.X/nbproject/Makefile-default.mk
+++ firmware/LTP_Board/LTP_Board_V1.X/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/interrupt_manager.c mcc_generated_files/traps.c mcc_generated_files/mcc.c mcc_generated_files/pin_manager.c mcc_generated_files/mccp1_compare.c mcc_generated_files/uart1.c mcc_generated_files/mssp1_spi.c mcc_generated_files/mssp2_i2c.c main.c LTP_system.c encoder.c lidar.c mcc_generated_files/tmr1.c motor.c
+SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/interrupt_manager.c mcc_generated_files/traps.c mcc_generated_files/mcc.c mcc_generated_files/pin_manager.c mcc_generated_files/mccp1_compare.c mcc_generated_files/uart1.c mcc_generated_files/mssp1_spi.c mcc_generated_files/mssp2_i2c.c main.c LTP_system.c encoder.c lidar.c mcc_generated_files/tmr1.c motor.c PID.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o ${OBJECTDIR}/mcc_generated_files/traps.o ${OBJECTDIR}/mcc_generated_files/mcc.o ${OBJECTDIR}/mcc_generated_files/pin_manager.o ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o ${OBJECTDIR}/mcc_generated_files/uart1.o ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o ${OBJECTDIR}/main.o ${OBJECTDIR}/LTP_system.o ${OBJECTDIR}/encoder.o ${OBJECTDIR}/lidar.o ${OBJECTDIR}/mcc_generated_files/tmr1.o ${OBJECTDIR}/motor.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o.d ${OBJECTDIR}/mcc_generated_files/traps.o.d ${OBJECTDIR}/mcc_generated_files/mcc.o.d ${OBJECTDIR}/mcc_generated_files/pin_manager.o.d ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o.d ${OBJECTDIR}/mcc_generated_files/uart1.o.d ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o.d ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/LTP_system.o.d ${OBJECTDIR}/encoder.o.d ${OBJECTDIR}/lidar.o.d ${OBJECTDIR}/mcc_generated_files/tmr1.o.d ${OBJECTDIR}/motor.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o ${OBJECTDIR}/mcc_generated_files/traps.o ${OBJECTDIR}/mcc_generated_files/mcc.o ${OBJECTDIR}/mcc_generated_files/pin_manager.o ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o ${OBJECTDIR}/mcc_generated_files/uart1.o ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o ${OBJECTDIR}/main.o ${OBJECTDIR}/LTP_system.o ${OBJECTDIR}/encoder.o ${OBJECTDIR}/lidar.o ${OBJECTDIR}/mcc_generated_files/tmr1.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/PID.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o.d ${OBJECTDIR}/mcc_generated_files/traps.o.d ${OBJECTDIR}/mcc_generated_files/mcc.o.d ${OBJECTDIR}/mcc_generated_files/pin_manager.o.d ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o.d ${OBJECTDIR}/mcc_generated_files/uart1.o.d ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o.d ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/LTP_system.o.d ${OBJECTDIR}/encoder.o.d ${OBJECTDIR}/lidar.o.d ${OBJECTDIR}/mcc_generated_files/tmr1.o.d ${OBJECTDIR}/motor.o.d ${OBJECTDIR}/PID.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o ${OBJECTDIR}/mcc_generated_files/traps.o ${OBJECTDIR}/mcc_generated_files/mcc.o ${OBJECTDIR}/mcc_generated_files/pin_manager.o ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o ${OBJECTDIR}/mcc_generated_files/uart1.o ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o ${OBJECTDIR}/main.o ${OBJECTDIR}/LTP_system.o ${OBJECTDIR}/encoder.o ${OBJECTDIR}/lidar.o ${OBJECTDIR}/mcc_generated_files/tmr1.o ${OBJECTDIR}/motor.o
+OBJECTFILES=${OBJECTDIR}/mcc_generated_files/interrupt_manager.o ${OBJECTDIR}/mcc_generated_files/traps.o ${OBJECTDIR}/mcc_generated_files/mcc.o ${OBJECTDIR}/mcc_generated_files/pin_manager.o ${OBJECTDIR}/mcc_generated_files/mccp1_compare.o ${OBJECTDIR}/mcc_generated_files/uart1.o ${OBJECTDIR}/mcc_generated_files/mssp1_spi.o ${OBJECTDIR}/mcc_generated_files/mssp2_i2c.o ${OBJECTDIR}/main.o ${OBJECTDIR}/LTP_system.o ${OBJECTDIR}/encoder.o ${OBJECTDIR}/lidar.o ${OBJECTDIR}/mcc_generated_files/tmr1.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/PID.o
 
 # Source Files
-SOURCEFILES=mcc_generated_files/interrupt_manager.c mcc_generated_files/traps.c mcc_generated_files/mcc.c mcc_generated_files/pin_manager.c mcc_generated_files/mccp1_compare.c mcc_generated_files/uart1.c mcc_generated_files/mssp1_spi.c mcc_generated_files/mssp2_i2c.c main.c LTP_system.c encoder.c lidar.c mcc_generated_files/tmr1.c motor.c
+SOURCEFILES=mcc_generated_files/interrupt_manager.c mcc_generated_files/traps.c mcc_generated_files/mcc.c mcc_generated_files/pin_manager.c mcc_generated_files/mccp1_compare.c mcc_generated_files/uart1.c mcc_generated_files/mssp1_spi.c mcc_generated_files/mssp2_i2c.c main.c LTP_system.c encoder.c lidar.c mcc_generated_files/tmr1.c motor.c PID.c
 
 
 CFLAGS=
@@ -186,6 +186,13 @@ ${OBJECTDIR}/motor.o: motor.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE)  motor.c  -o ${OBJECTDIR}/motor.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/motor.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off  
 	@${FIXDEPS} "${OBJECTDIR}/motor.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
+${OBJECTDIR}/PID.o: PID.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PID.o.d 
+	@${RM} ${OBJECTDIR}/PID.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  PID.c  -o ${OBJECTDIR}/PID.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/PID.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/PID.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
 else
 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.o: mcc_generated_files/interrupt_manager.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
@@ -284,6 +291,13 @@ ${OBJECTDIR}/motor.o: motor.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/motor.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  motor.c  -o ${OBJECTDIR}/motor.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/motor.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off  
 	@${FIXDEPS} "${OBJECTDIR}/motor.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
+${OBJECTDIR}/PID.o: PID.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PID.o.d 
+	@${RM} ${OBJECTDIR}/PID.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  PID.c  -o ${OBJECTDIR}/PID.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/PID.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/PID.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
