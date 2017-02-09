821088e9a30873ae9b265c4f8742fb4a768ca8c0
Paul Sammut <paul.c.sammut@gmail.com>
2017-02-08 19:10:11 -0500

    getting super weird behaviour :(

--- utils/serial_util/main.cpp
+++ utils/serial_util/main.cpp
@@ -9,38 +9,66 @@ int main(int argc, char *argv[])
 {
     unsigned char tempBuff[255];
     int bytesRead = 0;
-    unsigned char packetBuff[255];
     int packetLength = 0;
+    // Here we make sure that the packet was read from start to finish.
+    bool packetClean = false;
 
     char port[] = "/dev/ttyUSB0";
-    printf("Hello!");
+    printf("Ummm Hello!\r\n");
+
 
     if(serialOpen(port, _B230400) < 0)
         exit (0);
 
 
-    while(1) {
-        bytesRead = serialRead(tempBuff+packetLength,255,false);
-        
-//        std::copy(tempBuff, tempBuff+bytesRead, packetBuff + packetLength);
-       packetLength += bytesRead;
+    serialRead(tempBuff,128,false);
 
-        for(int i = packetLength-bytesRead; i < packetLength; i++)
+    while(1) {
+        // std::cout << std::flush;
+
+        if(packetLength >= 255)
         {
-            if(tempBuff[i] == DELIMITER)
+            std::cout << "Buffer overflow" << std::endl;
+            packetClean = false;
+            packetLength = 0;
+        }
+
+        bytesRead = serialRead(tempBuff+packetLength,128,false);
+
+        packetLength += bytesRead;
+
+        if(packetLength >= 255)
+        {
+            std::cout << "Buffer overflow" << std::endl;
+            packetClean = false;
+            packetLength = 0;
+        } else {
+            // We scan the "just read portion" for a delimeter
+            for(int i = packetLength-bytesRead; i < packetLength; i++)
             {
-                std::cout << "Yay we got a message!: " << packetLength << " : " ;
-                // end of message!
-                // take what we have so far and f
-               
-                tempBuff[packetLength] = 0;
-                // reset the packet length
-                std::cout << tempBuff << std::endl;
-                packetLength = 0;
+                if(tempBuff[i] == DELIMITER)
+                {
+                    //std::cout << "Yay we got a packet!: " << packetLength << " : " ;
+
+                    // set the end of the packet to 0, which terminates the string
+                    tempBuff[packetLength] = 0;
+
+                    if(packetClean)
+                    {
+                        //std::cout << tempBuff << std::endl;
+                        //return 1;
+                        if(packetLength != 8)
+                        {
+                            std::cout << tempBuff << std::endl;
+                        }
+                    }
+
+                    // reset the packet length
+                    packetLength = 0;
+                    packetClean = true;
+                }
             }
         }
-        //iterate through tempBuff and see what we got
-
     }
 
     serialClose();
